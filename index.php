1bda2be46cc0e495eca7b7ebb9a35045ab07a068
qinuoyun <qinuoyun@qq.com>
2018-01-29 21:20:40 +0800

    no message

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+namespace conveyancer\app;
+// Autoload 自动载入
+require './vendor/autoload.php';
+
+use \conveyancer\framework\kernel;
+
+(new kernel())->start();
+
+tool::test();
+
+tool::test();
+
+logger::test();
+
+tool::test();
+tool::test();
+tool::test();
+tool::test();
+
+// for ($i = 0; $i < 10000; $i++) {
+//     tool::test();
+
+//     logger::test();
+// }
+
+$t2 = microtime(true);
+echo '耗时' . round($t2 - START_TIME, 3) . '秒<br>';
+echo 'Now memory_get_usage: ' . memory_get_usage() . '<br />';
